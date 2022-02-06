b00afe6dab8b7f265f223b95e98c84fb33b5b00b
Aravind-SY <93550319+Aravind-SY@users.noreply.github.com>
2022-02-06 22:46:31 +0530

    Add files via upload

---
+++ Ques 1/src/com/greatlearning/company/main/Main.java
@@ -0,0 +1,12 @@
+package com.greatlearning.company.main;
+
+import com.greatlearning.company.model.skyscraper;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		skyscraper skyscraperService = new skyscraper();
+		skyscraperService.implementSkyscraperApp();
+	}
+
+}
