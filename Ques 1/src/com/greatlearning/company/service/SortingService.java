2c2216ae4167ec875b21b6a0b16d5566d239e0e3
Aravind-SY <93550319+Aravind-SY@users.noreply.github.com>
2022-02-19 00:30:01 +0530

    Update SortingService.java

--- Ques 1/src/com/greatlearning/company/service/SortingService.java
+++ Ques 1/src/com/greatlearning/company/service/SortingService.java
@@ -4,40 +4,28 @@ public class SortingService {
 	
 	public static void sort(int[] arr, int l, int r) {
 		if (l < r) {
-			// find the middle index
+			
 			int m = l + (r - l) / 2;
-
-			// sort the left half
 			sort(arr, l, m);
-
-			// sort the right half
 			sort(arr, m + 1, r);
-
-			// merge the sorted halves
 			merge(arr, l, m, r);
 		}
 	}
 
-	// method to merge the array
 	private static void merge(int[] arr, int l, int m, int r) {
-		// find sizes of 2 sub-arrays
 		int n1 = m - l + 1;
 		int n2 = r - m;
 
-		// temporary arrays to hold the halves
 		int[] L = new int[n1];
 		int[] R = new int[n2];
 
-		// copy data to sub-arrays
 		for (int i = 0; i < n1; ++i)
 			L[i] = arr[l + i];
 		for (int j = 0; j < n2; ++j)
 			R[j] = arr[m + 1 + j];
 
-		// initialize index for both the sub-arrays
 		int i = 0, j = 0;
 
-		// initialize index of merged array
 		int k = l;
 
 		while (i < n1 && j < n2) {
@@ -51,14 +39,12 @@ public class SortingService {
 			k++;
 		}
 
-		// copy remaining elements in L[] if any
 		while (i < n1) {
 			arr[k] = L[i];
 			i++;
 			k++;
 		}
 
-		// copy remaining elements in R[] if any
 		while (j < n2) {
 			arr[k] = R[j];
 			j++;
